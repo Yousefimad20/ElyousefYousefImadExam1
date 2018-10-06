1788276e96d01775f12bbd349d36c2010755ca2f
jaspervanbrian <2015081446@ust-ics.mygbiz.com>
2018-10-06 18:11:57 +0800

    Initial commit

---
+++ app/src/main/java/yousefimad/elyousef/com/elyousefyousefimadexam1/Exam1.java
@@ -0,0 +1,13 @@
+package yousefimad.elyousef.com.elyousefyousefimadexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam1);
+    }
+}
